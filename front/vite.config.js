502f1ffe37acad293092b383deccdc9e7e95eb45
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-14 11:53:04 +0530

    edit for vercel deployment

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/password_manager",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Expose on all network interfaces
-    port: 5173, // Choose a port (default is 5173)
-  },
 });
