97d215b08dc6ddefaa0936ac8c74c42b5006ecc8
Raghuveer Sharma <sharmaraghu157@gmail.com>
2025-05-14 11:18:24 +0530

    add social media links.

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+import { TbBrandLeetcode } from "react-icons/tb";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-800 p-2 text-white  w-full">
+    <footer className="bg-slate-800 p-2 text-white w-full">
       <div className="flex flex-col items-center max-w-5xl mx-auto space-y-3">
         {/* Logo */}
-        <div className="logo font-bold text-2xl md:text-4xl flex justify-center md:justify-start">
+        <div className="logo font-bold text-2xl md:text-3xl flex justify-center md:justify-start">
           <span className="text-purple-600">&lt;</span>
           pass
           <span className="text-purple-600">OP/&gt;</span>
         </div>
 
-        {/* Developer Link */}
-        <div className="text-xs md:text-sm underline hover:scale-105 cursor-pointer text-center">
-          <a
-            href="https://www.linkedin.com/in/raghuveer-sharma-810124252/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="transition-transform duration-200 hover:text-purple-400"
-          >
-            Raghuveer Sharma
-          </a>
+        {/* Social Media Icons - Responsive */}
+        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+          {[
+            {
+              icon: <FaLinkedin />,
+              link: "https://www.linkedin.com/in/raghuveer-sharma-810124252/",
+            },
+            {
+              icon: <FaInstagram />,
+              link: "https://www.instagram.com/__raghusharma__?igsh=MjkwYTRxb3ljaXBx",
+            },
+            {
+              icon: <FaGithub />,
+              link: "https://github.com/raghuveeersharma",
+            },
+            {
+              icon: <TbBrandLeetcode />,
+              link: "https://leetcode.com/u/RaghuSharma48/",
+            },
+          ].map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
+            >
+              {item.icon}
+            </a>
+          ))}
         </div>
+        {/* Name / Logo */}
+        <h2 className="text-lg font-semibold text-purple-400">
+          Raghuveer Sharma
+        </h2>
       </div>
     </footer>
   );
